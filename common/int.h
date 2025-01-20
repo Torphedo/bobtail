2fd1c8dbebd483c96acfe8a45bd661d756014c72
torph <73564623+Torphedo@users.noreply.github.com>
2025-01-20 12:56:11 -0500

    Add function to reflect over a line

--- common/int.h
+++ common/int.h
@@ -50,6 +50,14 @@ s32 s24_to_s32(u8* addr);
 /// Raise a number to a power (val^pow)
 u64 exponent(u32 val, u8 pow);
 
+/// @brief Reflect a value over a line
+///
+/// For example, reflect(0.3f, 0.5f) == 0.7f, because there's a 0.2f distance.
+/// Useful for fixing "upside-down" coordinates.
+/// @param val The value to reflect
+/// @param axis The position of a line to reflect over
+float reflect(float val, float axis);
+
 /// Print a 16-bit string as if it were 8-bit (portable, unlike %ws)
 void print_c16s(const c16* str);
 
