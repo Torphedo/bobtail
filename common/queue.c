0c00a33090e1b3c0d5cbd596978ee10a779a5e33
torph <73564623+Torphedo@users.noreply.github.com>
2025-09-30 18:54:01 -0400

    Generic queue

--- common/queue.c
+++ common/queue.c
@@ -1,13 +1,12 @@
 #include <string.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
 #include "logging.h"
 #include "queue.h"
 
 u32 queue_maxidx(queue q) {
-    const u32 element_size = (q.alloc_size / sizeof(*q.data));
-    return MAX(0, ((s32)element_size) - 1);
+    const s32 num_elements = (s32)q.buf.alloc_size / q.element_size;
+    return MAX(0, num_elements - 1);
 }
 
 bool queue_fullback(queue q) {
@@ -22,9 +21,18 @@ bool queue_empty(queue q) {
     return (q.front_idx == q.back_idx);
 }
 
-bool queue_contains(queue* q, queue_element val) {
+const void* queue_element_const(const queue* q, u16 idx) {
+    return (const void*)((uintptr_t)q->buf.data + (idx * q->element_size));
+}
+
+void* queue_element(queue* q, u16 idx) {
+    return (void*)((uintptr_t)q->buf.data + (idx * q->element_size));
+}
+
+bool queue_contains(const queue* q, void* val) {
     for (u32 i = q->front_idx; i < q->back_idx + 1; i++) {
-        if (q->data[i] == val) {
+        const void* element = queue_element_const(q, i);
+        if (memcmp(element, val, q->element_size) == 0) {
             return true;
         }
     }
@@ -32,90 +40,81 @@ bool queue_contains(queue* q, queue_element val) {
     return false;
 }
 
-queue queue_create(u32 init_size) {
+queue queue_create(u32 init_size, u16 element_size) {
     return (queue) {
-        .data = calloc(1, init_size),
-        .alloc_size = init_size,
+        .buf = buf_create(init_size * element_size, true),
+        .element_size = element_size,
         .front_idx = 0,
         .back_idx = 0
     };
 }
 
-void queue_add(queue* q, queue_element val) {
+void queue_add(queue* q, const void* val) {
     // If there's no room in the back
     if (queue_fullback(*q)) {
-        if (!queue_fullfront(*q) && q->data != NULL) {
+        if (!queue_fullfront(*q) && q->buf.data != NULL) {
             // We have open space at the front, so it can be transparently
             // reclaimed to make room for the new value.
-            const u32 size = (q->back_idx - q->front_idx) * sizeof(*q->data);
-            memmove(q->data, &q->data[q->front_idx], size);
+            const u32 size = (q->back_idx - q->front_idx) * q->element_size;
+            memmove(q->buf.data, queue_element(q, q->front_idx), size);
             // Zero out the now unused memory
-            memset(((u8*)q->data) + size, 0x00, q->alloc_size - size);
+            memset(((u8*)q->buf.data) + size, 0x00, q->buf.alloc_size - size);
 
             // The old front idx is also the number of open spaces we just
             // re-used. So the new back idx is just moved back by that amount.
             q->back_idx -= q->front_idx;
             q->front_idx = 0;
-        }
-        else {
+        } else {
             // The buffer is completely full & needs a new allocation.
             // Grow by 50%, rounded up to the next multiple of our data size.
-            const u32 newsize = ALIGN_UP((u32)(q->alloc_size * 1.5), sizeof(*q->data));
-            void* newbuf = calloc(1, newsize);
-            if (newbuf == NULL) {
-                LOG_MSG(error, "Couldn't expand queue 0x%X -> 0x%X [alloc failure]\n", q->alloc_size, newsize);
+            const u32 newsize = q->buf.alloc_size * 1.5;
+            if (!buf_resize(&q->buf, newsize, true)) {
+                LOG_MSG(error, "Couldn't expand queue 0x%X -> 0x%X [alloc failure]\n", q->buf.alloc_size, newsize);
                 return;
             }
-
-            // Copy data & update state
-            memcpy(newbuf, q->data, q->alloc_size);
-            free(q->data);
-            q->data = newbuf;
-            q->alloc_size = newsize;
         }
     }
 
     // Put value in the next slot
-    q->data[q->back_idx++] = val;
+    void* dest = queue_element(q, q->back_idx++);
+    memcpy(dest, val, q->element_size);
 }
 
-queue_element queue_peek(queue q) {
+bool queue_peek(queue q, void* val_out) {
+    if (queue_empty(q) || queue_maxidx(q) <= q.front_idx) {
+        return false;
+    }
     // We can't get a value if there's not enough buffer space for the read
-    if (q.alloc_size < sizeof(queue_element) || q.data == NULL) {
-        return 0;
+    if (q.buf.alloc_size < q.element_size || q.buf.data == NULL) {
+        return false;
     }
 
-    return q.data[q.front_idx];
+    memcpy(val_out, queue_element_const(&q, q.front_idx), q.element_size);
+    return true;
 }
 
-queue_element queue_get(queue* q) {
+bool queue_get(queue* q, void* val_out) {
     // Get top and increment idx
-    const queue_element val = queue_peek(*q);
-    if (val == 0) {
-        // TODO: Does returning 0 make sense here? 0 is a perfectly valid value
-        return 0;
+    if (!queue_peek(*q, val_out)) {
+        return false;
     }
     q->front_idx++;
 
     // If this was the last item, reset so the entire buffer can be re-used.
-    if (q->front_idx == q->back_idx) {
+    if (queue_empty(*q)) {
         q->front_idx = 0;
         q->back_idx = 0;
     }
-    return val;
+    return true;
 }
 
 void queue_clear(queue* q) {
-    memset(q->data, 0x00, q->alloc_size);
+    buf_clear(&q->buf);
     q->front_idx = 0;
     q->back_idx = 0;
 }
 
 void queue_destroy(queue* q) {
-    // Wipe all data
-    memset(q->data, 0, q->alloc_size);
-
-    void* data = q->data;
-    *q = (queue){0}; // Wipe structure
-    free(data); // Free backing buffer
+    buf_destroy(&q->buf);
+    *q = (queue){0};
 }
