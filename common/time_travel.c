40672f1d7e5d3a98ec2f8aab2223756d8c0a82eb
torph <73564623+Torphedo@users.noreply.github.com>
2026-06-21 08:36:21 -0400

    Add snapshot system to time travel regions

--- common/time_travel.c
+++ common/time_travel.c
@@ -1,7 +1,8 @@
 /// @author Torphedo
 #include "time_travel.h"
 #include <stdlib.h>
-#include <common/vmem.h>
+#include "vmem.h"
+
 
 enum {
     DIRTY_MAX = 256,
@@ -11,21 +12,23 @@ enum {
 typedef struct {
     void* pagebuf; // Buffer to store pages that changed between snapshots
     u32 page_count; // The number of changed pages
+    // How many snapshots in a row the state remained unchanged
+    u32 repeat_count;
     void* page_targets[]; // The addresses to store each page to apply the diff
-}tt_snapshot_record;
+}tt_state_diff;
 
-/// Create a diff between snapshots using a list of dirty pages
+/// Create a diff between states using a list of dirty pages
 /// @param dirty An array of pointers to dirty pages
 /// @param pages The number of dirty pages
-/// @return The snapshot diff
-tt_snapshot_record* tt_allocate_snapshot_record(void** dirty, u32 pages) {
-    tt_snapshot_record* out = calloc(1, sizeof(*out) + pages * sizeof(*out->page_targets));
+tt_state_diff* tt_state_allocate(void** dirty, u32 pages) {
+    tt_state_diff* out = calloc(1, sizeof(*out) + pages * sizeof(*out->page_targets));
     if (!out) {
         return NULL;
     }
     if (pages == 0) {
         return out;
     }
+    out->repeat_count = 1;
     out->page_count = pages;
     out->pagebuf = malloc(pages * vmem_get_page_size());
     if (!out->pagebuf) {
@@ -42,31 +45,56 @@ tt_snapshot_record* tt_allocate_snapshot_record(void** dirty, u32 pages) {
     return out;
 }
 
-void tt_destroy_snapshot_record(tt_snapshot_record* snap) {
-    free(snap->pagebuf);
-    free(snap);
+void tt_state_destroy(tt_state_diff* diff) {
+    free(diff->pagebuf);
+    free(diff);
 }
 
-/// Get a snapshot diff at a specific index
-tt_snapshot_record* tt_get_snapshot(tt_region* region, s32 id) {
-    return *(tt_snapshot_record**)list_get_element(region->snapshots, id);
+tt_state_diff* tt_get_state(tt_region* region, s32 id) {
+    if (id == 0) {
+        return NULL;
+    }
+    return *(tt_state_diff**)list_get_element(region->states, id);
 }
 
-/// Advance the state by 1 snapshot
+s32 tt_last_state(const tt_region* region) {
+    return list_size(region->states);
+}
+
+/// Advance the state by 1
 void tt_snapshot_advance(tt_region* region) {
-    const s32 target = region->cur_snapshot + 1;
-    if (target >= list_size(region->snapshots)) {
+    const s32 target = region->cur_state + 1;
+    if (target >= list_size(region->states)) {
         return; // Nothing to advance to
     }
 
     // Find the diff that goes from the current snapshot to the next one
-    const tt_snapshot_record* snap = tt_get_snapshot(region, target);
+    const tt_state_diff* diff = tt_get_state(region, target);
 
     // Apply the diff by copying each changed page
-    for (u32 j = 0; j < snap->page_count; j++) {
-        uintptr_t source = (uintptr_t)snap->pagebuf + (j * vmem_get_page_size());
-        memcpy(snap->page_targets[j], (const void*)source, vmem_get_page_size());
+    for (u32 j = 0; j < diff->page_count; j++) {
+        uintptr_t source = (uintptr_t)diff->pagebuf + (j * vmem_get_page_size());
+        memcpy(diff->page_targets[j], (const void*)source, vmem_get_page_size());
     }
+    region->cur_state = target;
+}
+
+s32 tt_snapshot_to_state(const tt_region* region, s32 snapshot) {
+    if (snapshot == 0) {
+        return 0;
+    }
+
+    for (s32 i = 0; i < list_size(region->states); i++) {
+        const tt_state_diff* diff = tt_get_state((tt_region*)region, i);
+        if (!diff) {
+            continue;
+        }
+        if (snapshot <= diff->repeat_count) {
+            return i;
+        }
+        snapshot -= diff->repeat_count;
+    }
+    return -1;
 }
 
 tt_region tt_allocate(u64 size) {
@@ -76,28 +104,28 @@ tt_region tt_allocate(u64 size) {
         .buf = vmem_alloc_watched(num_pages),
     };
 
-    list_create(10, sizeof(tt_snapshot_record*));
+    list_create(10, sizeof(tt_state_diff*));
 }
 
 void tt_destroy(tt_region* region) {
-    for (u32 i = 0; i < list_size(region->snapshots); i++) {
-        tt_snapshot_record* snap = tt_get_snapshot(region, i);
-        tt_destroy_snapshot_record(snap);
+    for (u32 i = 0; i < list_size(region->states); i++) {
+        tt_state_diff* diff = tt_get_state(region, i);
+        tt_state_destroy(diff);
     }
-    list_destroy(&region->snapshots);
+    list_destroy(&region->states);
 
-    free(region->snapshot_base);
+    free(region->state_base);
     vmem_free(region->buf, region->size);
 }
 
 s32 tt_snapshot(tt_region* region) {
-    if (!region->snapshot_base) {
+    if (!region->state_base) {
         // Copy the entire region as an initial state we can diff against
-        region->snapshot_base = malloc(region->size);
-        if (!region->snapshot_base) {
+        region->state_base = malloc(region->size);
+        if (!region->state_base) {
             return -1;
         }
-        memcpy(region->snapshot_base, region->buf, region->size);
+        memcpy(region->state_base, region->buf, region->size);
         vmem_reset_write_watching(region->buf, region->size);
         return 0;
     }
@@ -106,33 +134,85 @@ s32 tt_snapshot(tt_region* region) {
     u64 num_dirty = 0;
     void* dirty_pages[DIRTY_MAX] = {};
     vmem_get_dirty_pages(region->buf, num_pages, dirty_pages, ARRAY_SIZE(dirty_pages), &num_dirty);
+    if (num_dirty == 0) {
+        // Nothing has changed.
+        // If the initial state was just saved, there may be no state diffs yet.
+        const s32 last = tt_last_state(region);
+        if (last > 0) {
+            tt_get_state(region, last)->repeat_count++;
+        }
+        return last;
+    }
 
-    const u32 idx = list_size(region->snapshots);
-    tt_snapshot_record* snap = tt_allocate_snapshot_record(dirty_pages, num_dirty);
-    list_add(&region->snapshots, &snap);
+
+    const u32 idx = list_size(region->states);
+    tt_state_diff* diff = tt_state_allocate(dirty_pages, num_dirty);
+    list_add(&region->states, &diff);
 
     vmem_reset_write_watching(region->buf, region->size);
     return idx;
 }
 
-void tt_revert(tt_region* region, s32 snapshot_id) {
-    if (snapshot_id == region->cur_snapshot) {
+void tt_restore_state(tt_region* region, s32 state) {
+    if (state == region->cur_state) {
         return; // Nothing to do
     }
 
-    if (snapshot_id < region->cur_snapshot) {
+    if (state < region->cur_state) {
         // We have to rewind, so revert to the initial state and then fast forward
-        memcpy(region->buf, region->snapshot_base, region->size);
-        region->cur_snapshot = 0;
+        memcpy(region->buf, region->state_base, region->size);
+        region->cur_state = 0;
     }
 
-    // Fast forward each snapshot until we reach the target
-    for (u32 i = region->cur_snapshot; i < snapshot_id; i++) {
+    // Fast forward each state until we reach the target
+    for (u32 i = region->cur_state; i < state; i++) {
         tt_snapshot_advance(region);
     }
 }
 
-void tt_revert_relative(tt_region* region, s32 diff) {
-    const s32 target = region->cur_snapshot + diff;
-    tt_revert(region, target);
+void tt_restore_state_relative(tt_region* region, s32 diff) {
+    const s32 target = region->cur_state + diff;
+    tt_restore_state(region, target);
+}
+
+s32 tt_current_state(const tt_region* region) {
+    return region->cur_state;
+}
+
+void tt_make_last_state(tt_region* region, s32 state) {
+    for (u32 i = state; i < list_size(region->states); i++) {
+        tt_state_diff* diff = tt_get_state(region, i);
+        if (diff) {
+            tt_state_destroy(diff);
+        }
+        list_remove(&region->states, i);
+    }
+
+    region->cur_state = tt_last_state(region);
+}
+
+bool tt_is_state_first(tt_region* region, s32 state) {
+    return state == 0;
+}
+
+bool tt_is_state_last(tt_region* region, s32 state) {
+    return state == tt_last_state(region);
+}
+
+void tt_restore_snapshot(tt_region* region, s32 snapshot) {
+    const s32 state = tt_snapshot_to_state(region, snapshot);
+    if (state >= 0) {
+        tt_restore_state(region, state);
+    }
+}
+
+u32 tt_snapshot_count(tt_region* region) {
+    u32 count = 0;
+    for (s32 i = 0; i < list_size(region->states); i++) {
+        const tt_state_diff* diff = tt_get_state(region, i);
+        if (diff) {
+            count += diff->repeat_count;
+        }
+    }
+    return count;
 }
--- common/time_travel.h
+++ common/time_travel.h
@@ -2,13 +2,22 @@
 #include "int.h"
 #include "list.h"
 
-// A region of memory which can be snapshotted and reverted to previous states
+/// @brief A memory region you can snapshot and restore to
+///
+/// There are 2 different sets of IDs you can use when restoring, *states* and
+/// *snapshots*. The *state* ID increments whenever something changes.
+/// The snapshot ID increments every time you take a snapshot, even if nothing
+/// changed.
+/// Restoring *states* is useful for an undo/redo system where you don't care
+/// how long you went between changes. Restoring *snapshots* is useful in
+/// replaying a simulation, when you need an accurate recreation of the state at
+/// each time step.
 typedef struct {
     void* buf;
     u64 size;
-    void* snapshot_base;
-    s32 cur_snapshot;
-    list snapshots;
+    void* state_base;
+    s32 cur_state;
+    list states;
 }tt_region;
 
 /// Allocate a time-travelable memory region
@@ -19,14 +28,31 @@ void tt_destroy(tt_region* region);
 
 /// Save a new snapshot of the region
 /// @param region The region to snapshot
-/// @return The ID of the snapshot
+/// @return The ID of the state
+/// (if nothing has changed, this may be the same as the last snapshot)
 s32 tt_snapshot(tt_region* region);
 
-// Revert to a specific snapshot
-void tt_revert(tt_region* region, s32 snapshot_id);
+/// Restore a specific state of memory
+void tt_restore_state(tt_region* region, s32 state);
 
-// Revert or fast-foward by @ref diff steps from the current state
-void tt_revert_relative(tt_region* region, s32 diff);
+/// Revert or fast-foward by @ref diff steps from the current state
+void tt_restore_state_relative(tt_region* region, s32 diff);
 
-// Get the snapshot ID of the current region state
-s32 tt_current_snapshot(const tt_region* region);
+// Get the current state ID
+s32 tt_current_state(const tt_region* region);
+
+
+
+// Make the given state the last one, wipe all states that come after
+void tt_make_last_state(tt_region* region, s32 state);
+
+bool tt_is_state_first(tt_region* region, s32 state);
+bool tt_is_state_last(tt_region* region, s32 state);
+
+// Snapshot ID increments on state changes and repeats
+// Hard to know if a full replay is needed unless we track the starting
+// snapshot ID at each state
+void tt_restore_snapshot(tt_region* region, s32 snapshot);
+
+u32 tt_snapshot_count(tt_region* region);
+
